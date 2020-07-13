3e625d3f237a5225fd8c4bb564f79a68dede5d39
amadi5892 <66728197+amadi5892@users.noreply.github.com>
2020-07-13 09:57:55 -0400

    Added Basket.jsx and Options.jsx

---
+++ src/components/Options.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Options() {
+    return(
+        <div>
+            <h1>Prodcuts</h1>
+        </div>
+    )
+}
+
+export default Options;
--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Basket from './Basket';
+import Options from './Options';
+
+const PROD_PAGE = 'products';
+const CART_PAGE = 'cart'; // naming this Basket
 
 function ShoppingCart() {
+    const [page, setPage] = useState(PROD_PAGE);
+
+    const toCart = (nextPage) => {
+        setPage(nextPage)
+    }
+
+    const renderCart = () => {
+        return(
+            <Basket />
+        )
+    }
+
+    const renderProd = () => {
+        return(
+            <Options />
+        )
+    }
+
     return (
         <div>
-            <h2>Products</h2>
-            
+            <button onClick={() => toCart(CART_PAGE)} >Cart</button>
+            <button onClick={() => toCart(PROD_PAGE)} >Prodcuts</button>
+            {page === PROD_PAGE && renderProd()}
+            {page === CART_PAGE && renderCart()}
         </div>
     )
 }

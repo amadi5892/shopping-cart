a8967a6b8fca11b77fda7ae043f4d23fc865b141
amadi5892 <66728197+amadi5892@users.noreply.github.com>
2020-07-13 17:10:36 -0400

    Deleted Basket.jsx and Options.jsx

--- src/components/ShoppingCart.jsx
+++ src/components/ShoppingCart.jsx
@@ -5,96 +5,97 @@ const PROD_PAGE = 'products';
 const CART_PAGE = 'cart';
 
 function ShoppingCart() {
+
     // Initially sets our site to the Product Page
     const [page, setPage] = useState(PROD_PAGE);
+
+    const [cart, setCart] = useState(0)
+
+    const [total, setTotal] = useState(0)
 
     // +++++++++++++++++++++CREATES PRODUCT PAGE VIEW+++++++++++++++++++
 
     // Structures our items on our product page
-    const Prod = () => {
+    const Prod = (props) => {
         return(
             <div className="item">
                 <div className="top-item">
-                <h3>Name</h3>
-                <h4>Price</h4>
+                <h3>{props.name}</h3>
+                <h4>{props.price}</h4>
                 </div>
 
                 <div className="mid-item">
-                    <p>Description</p>
+                    <p>{props.description}</p>
                 </div>
 
                 <div className="bottom-item">
-                    <button /*onClick={() => toCart(products)}*/ >Add to Cart</button>
+                    <button onClick={() => setCart(currentCart => (
+                        cart + 1
+                    ))} >Add to Cart</button>
                 </div>
             </div>
         )
     }
 
     // Creates the component for our Product Page
-    const createProd = () => {
+    const createProd = (product) => {
         return(
-            <Prod />
+            <Prod 
+            key={product.id}
+            name={product.name}
+            price={product.price}
+            description={product.description}
+            />
         )
     }
+
+    const [{items, price, total}, setCart] = useState({items: '', total: 0})
 
     // Renders Product Page
     const renderProd = () => {
         return(
             <div>
-            <div>
+            <div className="products">
                 <h1>Products</h1>
-                <p>Items in Cart: ()</p>
+                <p>Items in Cart: ({cart}) </p>
+                <p>Total: ({total}) </p>
             </div>
             <div className="grid">
                 {products.map(createProd)}
             </div>
+            <div className="checkout">
+                <div>
+                    <h1>Checkout</h1>
+                </div>
+                <div>
+                    <p>
+                        items: {items}
+                        <br></br>
+                        total: {total}
+                        <br></br>
+                        state taxes: {total * 0.08}
+                        <br></br>
+                        subtotal: {total + (total * 0.08)}
+                        <br></br>
+                    </p>
+                </div>
+            </div>
         </div>
         )
     }
 
-    // +++++++++++++++++++++CREATES CART PAGE VIEW+++++++++++++++++++++
 
-    // Structures our Cart items...HOPEFULLY
-    const Cart = () => {
-        return(
-            <div>
 
-            </div>
-        )
-    }
-
-    // Creates the components for our Cart Page
-    const createCart = () => {
-        return(
-            <Cart />
-        )
-    }
-
-    // Renders Cart Page
-    const renderCart = () => {
-        return(
-            <div>
-                <div>
-                    <h1>Cart</h1>
-                </div>
-            </div>
-        )
-    }
-
-    // Sends us from page to page
-    const toCart = (nextPage) => {
-        setPage(nextPage)
-    }
 
     // Renders it all!!!!
     return(
         <div>
-            <button onClick={() => toCart(CART_PAGE)} >Cart</button>
-            <button onClick={() => toCart(PROD_PAGE)} >Products</button>
+            <button >Checkout </button>
+            <button >Products</button>
             {page === PROD_PAGE && renderProd()}
-            {page === CART_PAGE && renderCart()}
         </div>
     )
+
 }
 
 export default ShoppingCart;

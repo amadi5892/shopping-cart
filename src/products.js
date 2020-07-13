69a6d33d12e66c0401725b19c0ef9977c51e9823
amadi5892 <66728197+amadi5892@users.noreply.github.com>
2020-07-13 09:25:56 -0400

    Added products.js and ShoppingCart.jsx

---
+++ src/products.js
@@ -0,0 +1,26 @@
+const products = [
+    {
+        id: 1,
+        name: "Tshirt",
+        price: 35,
+        description: "100% Cotton Tshirt",
+    },
+    {
+        id: 2,
+        name: "Hoodie",
+        price: 35,
+        description: "100% Cotton Tshirt",
+    },
+    {
+        id: 3,
+        name: "Sweat Pants",
+        price: 35,
+        description: "100% Cotton Tshirt",
+    },
+    {
+        id: 4,
+        name: "Hat",
+        price: 35,
+        description: "100% Cotton Tshirt",
+    }
+]
